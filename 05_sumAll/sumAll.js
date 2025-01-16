25bdb039ab185d82c3715d3b412ea5705a37c5ab
Kyle Liu <168138037+kyle2570@users.noreply.github.com>
2025-01-16 16:30:53 -0500

    Solved SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(numA,numB) {
+
+    let sum = 0;
+    let sortA = 0;
+    let sortB = 0;
+
+    if (numA > numB){
+        sortA = numB;
+        sortB = numA;
+    }else{
+        sortA = numA;
+        sortB = numB;
+
+    }
+
+    if ( !Number.isInteger(sortA)||!Number.isInteger(sortB)|| sortA < 0 || sortB <0){
+    return "ERROR";
+    } else{
+
+        for (let i = sortA; i <= sortB; i++){
+            sum = sum + i;
+        }
+        return sum;
+
+
+
+    }
+
+
 
 };
 
