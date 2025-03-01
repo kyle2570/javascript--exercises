bc98f8e21877d3fce352a996c80bf9fae6e05152
Kyle Liu <168138037+kyle2570@users.noreply.github.com>
2025-02-28 23:22:48 -0500

    Passed test #1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+    //   if (person.yearOfDeath == undefined) {
+
+    //   }
+
+    //   else {
+
+
+    const oldestPerson = people.reduce((oldest, person) =>
+
+        (person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ? person : oldest);
+
+
+    return oldestPerson;
+
+    //}
+
 
 };
 
